afd88cd9e219efd4529743008f2bd0298a020500
Polina Korolyeva <polina.koroleva@gmail.com>
2017-07-14 19:46:47 +0300

    fixed gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,41 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 var minifyCss = require('gulp-minify-css');
+var autoprefix = require('gulp-autoprefixer');
 
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: ''
+      baseDir: 'dist'
     },
   })
 });
 
+gulp.task('assets', function() {
+  gulp.src('src/index.html')
+    .pipe(gulp.dest('dist/'));
+
+  gulp.src('src/images/*')
+    .pipe(gulp.dest('dist/images/'));
+});
+
 gulp.task('less', function() {
-  gulp.src('css/*.less')
+  gulp.src('src/less/*.less')
     .pipe(less())
     .pipe(minifyCss())
-    .pipe(gulp.dest('css'))
+    .pipe(autoprefix(('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')))
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('dist/css/'))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
- 
+
 gulp.task('watch', ['browserSync', 'less'], function() {
-    gulp.watch('css/*.less', ['less']);
+    gulp.watch('src/less/*.less', ['less']);
     gulp.watch('*.html', browserSync.reload);
 })
  
-gulp.task('default', ['less', 'watch']);
+gulp.task('default', ['assets', 'less', 'watch']);
